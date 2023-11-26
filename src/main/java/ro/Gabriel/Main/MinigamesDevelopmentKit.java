15795bc32b9a5edfd3c6f727d6f4391cd24e5859
GabrielV99 <Gabriel_Vitza@yahoo.com>
2023-11-27 01:55:39 +0200

    .

--- src/main/java/ro/Gabriel/Main/MinigamesDevelopmentKit.java
+++ src/main/java/ro/Gabriel/Main/MinigamesDevelopmentKit.java
@@ -49,9 +49,9 @@ public class MinigamesDevelopmentKit extends JavaPlugin {
 //                    plugin.getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&1Aceasta este metoda: &5" + m));
 //
 //                }
-                ReflectionUtils.invokeMethod(plugin, true, JavaPlugin.class, "setEnabled", true);
+                //ReflectionUtils.invokeMethod(plugin, true, JavaPlugin.class, "setEnabled", true);
                 //ReflectionUtils.invokeMethod(plugin, true, "setEnabled", true);
-
+                ReflectionUtils.invokeMethod(plugin, "setEnabled", true);
                 plugin.getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&1S-a putut obtine clasa final &5PluginClassLoader"));
             } catch (Exception e) {
                 plugin.getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&1Nu s-a putut obtine clasa final &cPluginClassLoader"));
